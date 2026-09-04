2822562863a8e410e994ccec3ea195b2c9ba5e9e
Renato Marques <renatomardev@gmail.com>
2026-09-03 22:55:21 -0300

    feature: small auth refactoring

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,10 @@
-import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  Show,
-  UserButton,
-} from "@clerk/nextjs"
+import { ClerkProvider } from "@clerk/nextjs"
 import { shadcn } from "@clerk/ui/themes"
 import { Geist, Geist_Mono } from "next/font/google"
 
 import "./globals.css"
 import "@clerk/ui/themes/shadcn.css"
 import { ThemeProvider } from "@/components/theme-provider"
-import { Button } from "@/components/ui/button"
 import { Toaster } from "@/components/ui/sonner"
 import { cn } from "@/lib/utils"
 
@@ -41,19 +34,6 @@ export default function RootLayout({
       <body>
         <ClerkProvider appearance={{ theme: shadcn }}>
           <ThemeProvider>
-            <header className="flex items-center justify-end gap-3 border-b p-4">
-              <Show when="signed-out">
-                <SignInButton mode="modal">
-                  <Button variant="outline">Sign in</Button>
-                </SignInButton>
-                <SignUpButton mode="modal">
-                  <Button>Sign up</Button>
-                </SignUpButton>
-              </Show>
-              <Show when="signed-in">
-                <UserButton />
-              </Show>
-            </header>
             {children}
             <Toaster />
           </ThemeProvider>
